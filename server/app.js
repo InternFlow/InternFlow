469de0e111d96fe085427fd542d33bb3f0c46ca7
ZeinebHaraketi <zeineb.haraketi@esprit.tn>
2023-03-07 09:28:29 +0100

    email confirmation + github Done !

--- server/app.js
+++ server/app.js
@@ -1,33 +1,57 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const http = require('http');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
+
+
+
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-//Connecting to MongoDB
+// Connection to MongoDB
 mongoose.set('strictQuery',true);
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI , {useNewUrlParser: true})
 .then(()=>{ console.log("Connected to DB")})
-.catch((err)=>{console.log(err.message)});
-
-
+.catch((err)=>{console.log(err.message)})
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:5000/github"
+}, function(accessToken, refreshToken, profile, cb) {
+  // This function is called when the user is authenticated
+  // You can use the access token to make API calls to the GitHub API
+  // The user's GitHub profile information is available in the `profile` object
+  return cb(null, profile);
+}));
+
+app.get('/github', passport.authenticate('github'));
+
+app.get('/github',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to dashboard.
+    res.redirect('/');
+  });
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,9 +69,9 @@ app.use(function(err, req, res, next) {
   res.json('error');
 });
 
-//Creating HTTP Server
+//Connecting a Server
 const server = http.createServer(app);
-server.listen(process.env.PORT || 5000, ()=>{
-  console.log(`app is running on port ${process.env.PORT}`);
-});
+server.listen(process.env.PORT||5000, ()=>{
+  console.log(`App is running on localhost:${process.env.PORT}`);
+})
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const { Register, SearchUser } = require('../controllers/users');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json('it works');
+  res.json('it works!');
 });
+
+router.post('/register', Register);
+router.get('/search', SearchUser);
+
+
 
 module.exports = router;
